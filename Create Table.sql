65856231105d76874b9523593e68f3efe9c23bf5
Sado Nuuer <65812675+s06-n@users.noreply.github.com>
2024-05-20 14:17:46 +0100

    Add files via upload
    Uploads all files related to the Offuture Project

---
+++ Create Table.sql
@@ -0,0 +1,41 @@
+/*Offuture File1: Table Creation */
+--DROP TABLE team_having_offuture 
+
+CREATE TABLE team_having_offuture(
+"Row ID" INTEGER NULL,
+"Order ID" VARCHAR(50) NULL,
+"Order Date" VARCHAR(50) NULL,
+"Ship Date" VARCHAR(50) NULL,
+"Ship Mode" VARCHAR(50) NULL,
+"Customer ID" VARCHAR(50) NULL,
+"Customer Name" VARCHAR(50) NULL,
+"Segment" VARCHAR(50) NULL,
+"City" VARCHAR(50) NULL,
+"State" VARCHAR(50) NULL,
+"Country" VARCHAR(50) NULL,
+"Postal Code" VARCHAR(50) NULL,
+"Market" VARCHAR(50) NULL,
+"Region" VARCHAR(50) NULL,
+"Product ID" VARCHAR(50) NULL,
+"Category" VARCHAR(50) NULL,
+"Sub-Category" VARCHAR(50) NULL,
+"Product Name" VARCHAR(200) NULL,
+"Sales" NUMERIC NULL,
+"Quantity" INTEGER NULL,
+"Discount" NUMERIC(6,3) NULL,
+"Profit" NUMERIC NULL,
+"Shipping Cost" NUMERIC(5,2) NULL,
+"Order Priority" VARCHAR(50) NULL
+);
+
+SELECT *
+FROM team_having_offuture;
+
+
+
+
+
+
+
+
+
